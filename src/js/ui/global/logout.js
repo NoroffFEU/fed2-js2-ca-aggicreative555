0424b7a2d090dca672cffa5d2410285364818f6a
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2024-12-01 19:00:27 +0100

    feat(logout): add logout functionality

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,1 +1,26 @@
-export function setLogoutListener() {}
+import { onLogout } from "../auth/logout";
+
+/**
+ * Attaches a click event listener to the logout button to handle user logout.
+ *
+ * @function setLogoutListener
+ * @throws {Error} If the logout button is not found in the DOM.
+ *
+ */
+
+export function setLogoutListener() {
+    try {
+        const logoutButton = document.querySelector("#logoutButton");
+    
+        console.log(logoutButton);
+    
+        if (logoutButton) {
+            logoutButton.addEventListener("click", onLogout);
+        } else {
+            throw new Error("Logout button not found");
+        }
+    } catch (error) {
+        console.error("Logout listener not working."); 
+        throw error;
+    }
+}
